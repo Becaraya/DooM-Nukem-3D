2952df4655ffe78033e9e4659eb2e1b482445b2e
Benjamin CARAYANNIS <becaraya@e3r5p4.42.fr>
2019-11-06 16:20:03 +0100

    mise a jour de la libft

--- libft/ft_strjoin_free_f.c
+++ libft/ft_strjoin_free_f.c
@@ -1,18 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strjoin_free_f.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: becaraya <becaraya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/10 14:10:00 by becaraya          #+#    #+#             */
-/*   Updated: 2019/11/06 15:17:20 by becaraya         ###   ########.fr       */
+/*   Created: 2019/11/06 14:40:36 by becaraya          #+#    #+#             */
+/*   Updated: 2019/11/06 14:42:35 by becaraya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoinend(char const *s1, char const *s2)
+/*
+**free the first pointer 
+*/
+
+char	*ft_strjoinfreef(char *s1, char const *s2)
 {
 	char	*r;
 
@@ -22,23 +26,7 @@ char	*ft_strjoinend(char const *s1, char const *s2)
 			+ ft_strlen(s2) + 1))))
 			return (NULL);
 		ft_strcpy(r, s1);
-		ft_strcat(r, s2);
-		r[ft_strlen(s1) + ft_strlen(s2) + 1] = '\0';
-		return (r);
-	}
-	return (NULL);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*r;
-
-	if (s1 && s2)
-	{
-		if (!(r = (char *)malloc(sizeof(char) * (ft_strlen(s1)
-			+ ft_strlen(s2) + 1))))
-			return (NULL);
-		ft_strcpy(r, s1);
+		ft_strdel(&s1);
 		ft_strcat(r, s2);
 		return (r);
 	}

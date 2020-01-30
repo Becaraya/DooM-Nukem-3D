584ab3f19cd7246cbd002147785713ac6ffc216f
Yoann DEMANGEAU <ydemange@e1r9p11.42.fr>
2020-01-30 18:03:47 +0100

    choix texture

--- srcs/choose_tex.c
+++ srcs/choose_tex.c
@@ -6,7 +6,7 @@
 /*   By: becaraya <becaraya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/12 18:52:57 by becaraya          #+#    #+#             */
-/*   Updated: 2020/01/30 15:05:14 by ydemange         ###   ########.fr       */
+/*   Updated: 2020/01/30 18:03:14 by ydemange         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,24 @@ void		display_tex_menu(SDL_Surface *surf, t_tex tex, int i)
 	}
 }
 
+static void		set_tex(t_al *al)
+{
+	t_sector		*tmp_s;
+	t_walls			*tmp_w;
+	int				i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	tmp_s = al->sect;
+	while (al->edit.index_sect + i++ < al->nb_sec)
+		tmp_s = tmp_s->next;
+	tmp_w = tmp_s->walls;
+	while (j++ != al->edit.index_wall)
+		tmp_w = tmp_w->next;
+	tmp_w->wall_tex = al->tex_choice;
+}
+
 void		click_on_menu(t_al *al, SDL_Surface *surf)
 {
 	if (al->ev.type == SDL_MOUSEBUTTONDOWN && al->ev.motion.windowID == 2)
@@ -45,6 +63,8 @@ void		click_on_menu(t_al *al, SDL_Surface *surf)
 		&& al->ev.button.y <= (al->nb_tex + 1) * TEX_SIZE_MENU)
 		{
 			al->tex_choice = al->ev.button.y / TEX_SIZE_MENU;
+			set_tex(al);
+			al->edit.stat = SELECT;
 		}
 	}
 }
